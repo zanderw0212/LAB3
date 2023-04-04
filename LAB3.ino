bb4f170935253f453dfac8844e335fb02c8b7fbf
zanderw0212 <125565178+zanderw0212@users.noreply.github.com>
2023-04-04 11:09:53 +0100

    Add files via upload

---
+++ LAB3.ino
@@ -0,0 +1,249 @@
+
+#define task4LED 4
+#define signalT1 3
+#define task4Pot 8
+unsigned long time1;
+unsigned long time2;
+//task 2 perameters
+const int task2SW = 7;
+int freq2Val;
+float task2Tot;
+float task2Freq;
+float total2Freq;
+//task 3 perameters
+const int task3SW = 2;
+int freq3Val;
+float task3Tot;
+float task3Freq;
+float total3Freq;
+//task4 perameters
+unsigned int analogVal[4];
+int sumVoltage = 0;
+int voltage;
+float avarageVoltage = 0;
+static SemaphoreHandle_t semi
+static SemaphoreHandle_t semi2
+static SemaphoreHandle_t semi3
+
+//button LED
+#define buttonLED 9
+#define button 12
+
+
+
+
+
+void setup(){
+xTaskCreatePinnedToCore(
+            task1,
+            "task1",
+            4096,
+            NULL,
+            1,
+            NULL,
+            0);
+
+xTaskCreatePinnedToCore(
+            task2,
+            "task2",
+            4096,
+            NULL,
+            1,
+            NULL,
+            0);
+
+xTaskCreatePinnedToCore(
+            task3,
+            "task3",
+            4096,
+            NULL,
+            1,
+            NULL,
+            0);
+
+xTaskCreatePinnedToCore(
+            task4,
+            "task4",
+            4096,
+            NULL,
+            1,
+            NULL,
+            0);
+
+xTaskCreatePinnedToCore(
+            task5,
+            "task5",
+            4096,
+            NULL,
+            1,
+            NULL,
+            0);
+
+xTaskCreatePinnedToCore(
+            ButtonRead,
+            "Button Read",
+            4096,
+            NULL,
+            1,
+            NULL,
+            0);            
+
+xTaskCreatePinnedToCore(
+            ButtonWrite,
+            "Button Write",
+            4096,
+            NULL,
+            1,
+            NULL,
+            0);   
+  Serial.begin(9600);                
+  delayMicroseconds(2000);        
+  pinMode(signalT1,OUTPUT);
+  pinMode(task2SW, INPUT);  
+  pinMode(task3SW, INPUT);
+  pinMode (task4Pot, INPUT);
+  pinMode (task4LED, OUTPUT);
+  pinMode (buttonLED, OUTPUT);
+  pinMode (button, INPUT);
+  sema = xSemaphoreCreateBinary();
+  sema2 = xSemaphoreCreateBinary();
+  sema3 = xSemaphoreCreateBinary();
+}
+
+
+void loop(void){}
+
+
+void task1(void*parameter){
+while(1){
+
+    digitalWrite(signalT1, HIGH);
+     delayMicroseconds(200);
+    digitalWrite(signalT1, LOW);
+     delayMicroseconds(50);
+    digitalWrite(signalT1, HIGH);
+     delayMicroseconds(30);
+    digitalWrite(signalT1, LOW);
+
+      vTaskDelay(500/portTICK_PERIOD_MS)
+}
+
+}
+
+void task2(void*parameter){
+ 
+while(1){
+  
+  freq2Val = pulseIn(task2SW,HIGH,1000);
+  task2Tot = (freq2Val*2);
+  task2Freq = 1000000/task2Tot;
+
+  
+
+
+xSemaphoreGive(sema);
+
+}
+}
+
+
+
+void task3(void*parameter){
+
+ while(1){
+  
+  freq3Val = pulseIn(task3SW,HIGH,1000);
+  task3Tot = (freq3Val*2);
+  task3Freq = 1000000/task3Tot;
+
+
+xSemaphoreGive(sema2);
+
+
+ }
+}
+
+
+void task4(void*parameter){
+
+while(1){
+  for (int i = 0; i < 4; i++){
+  
+  analogVal[i] = analogRead(task4Pot);
+  
+  }
+
+  for ( int i = 0; i < 4; i++){
+
+    sumVoltage += analogVal[i]; 
+  }
+
+  avarageVoltage = sumVoltage/4;
+
+  voltage = avarageVoltage*(3.3/4095.0);
+
+  if (voltage >= 1.65){
+    
+   digitalWrite (task4LED, HIGH);
+    
+  }
+
+  else {
+    
+  digitalWrite (task4LED, LOW);    
+
+  }
+
+  
+  Serial.print(voltage);
+  Serial.print('V');
+
+  if (avarageVoltage > 2047){
+    Serial.print ('Voltage too high');
+  }
+}
+}
+
+
+
+void task5(void*parameter){
+while(1){
+xSemaphoreTake(sema,0);
+xSemaphoreTake(sema2,0);
+  int Frequancy2task5 = map(task2Freq, 333, 1000, 0, 99);
+  int Frequancy3task5 = map(task3Freq, 500, 1000, 0, 99);
+
+  Frequancy2task5 = constrain(Frequancy2task5, 0, 99);
+  Frequancy3task5 = constrain(Frequancy3task5, 0, 99);
+
+Serial.print(Frequancy2task5);
+Serial.print(Frequancy3task5);
+
+
+  }
+}
+
+void buttonRead (void*parameter){
+  while (1){
+
+  buttonPress = digitalRead (buttton);
+
+
+  xSemaphoreGive(sema3);
+  }
+  
+}
+
+void buttonWrite(void*parameter){
+while (1){
+xSemaphoreTake (sema3,0);
+
+  if (buttonPress = HIGH){
+    digitalWrite ()
+  }
+  
+}  
+}
+
+
+
